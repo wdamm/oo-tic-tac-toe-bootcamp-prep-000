885514f9ed866c76aa232bf1970180da6c728e3c
William Damm <wdamm@powerhrg.com>
2020-04-22 20:54:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,9 @@ def position_taken?(location)
   @board[location] != " " && @board[location] != ""
 end
 
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
   
   
 end
