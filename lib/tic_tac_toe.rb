09a5c2a23d5bef1aa96c0af703f55ab0802d29d1
William Damm <wdamm@powerhrg.com>
2020-04-22 20:38:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,6 +6,10 @@ class TicTacToe
   
  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   
+  def board
+    @board
+  end
+  
 def display_board(board)
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
