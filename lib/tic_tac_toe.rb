5c1b7b0e8403d721797d4f185d70587120ff52af
William Damm <wdamm@powerhrg.com>
2020-04-22 20:53:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,7 @@ class TicTacToe
     @board
   end
   
-def display_board(board)
+def display_board()
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
